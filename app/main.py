5967a0ae19a6b1548b21e90d69a46bab9739b80e
unknown <turlay03102008@gmail.com>
2026-04-19 15:31:14 +0300

    add register and login endpoints, fix router architecture

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from app.db.session import engine, Base
 from app.api.v1 import cars 
+from app.api.v1.auth.register import router as auth_router
+from app.api.v1.auth.login import router as login_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(auth_router, prefix="/api/v1/auth", tags=["Auth"])
+app.include_router(login_router, prefix="/api/v1/auth", tags=["Auth"])
 app.include_router(cars.router, prefix="/api/v1", tags=["Cars"])
 
 @app.get("/")
