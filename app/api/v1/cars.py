9db26baa70958e8d980f72758027f0aee1d7fdcb
unknown <turlay03102008@gmail.com>
2026-04-29 16:08:54 +0300

    update router /cars

--- app/api/v1/cars.py
+++ app/api/v1/cars.py
@@ -11,7 +11,14 @@ router = APIRouter()
 
 @router.get("/cars", response_model=list[CarResponse])
 def get_all_cars(db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
-    db_cars = db.query(models.Car).all()
+    active_trips = db.query(Trip.car_id).filter(Trip.status == "active").all()
+    active_car_ids = [trip[0] for trip in active_trips]
+
+    if active_car_ids:
+        db_cars = db.query(models.Car).filter(models.Car.id.notin_(active_car_ids)).all()
+    else:
+        db_cars = db.query(models.Car).all()
+
     result = []
     for car in db_cars:
         car_dict = {
